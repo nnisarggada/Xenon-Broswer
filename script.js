b1c1868764d555c14cbd4b7311ae4364a9b47b75
Nnisarg Gada <gadannisarg@gmail.com>
2023-03-30 02:03:39 +0530

    yes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("browser").addEventListener("dom-ready",  function (e) {
+document.getElementById("browser").addEventListener("dom-ready", function (e) {
   document.getElementById("browser").insertCSS(`
     ::-webkit-scrollbar{
         width: 7px;
@@ -21,47 +21,40 @@ document.getElementById("browser").addEventListener("dom-ready",  function (e) {
         background-color: #505050;
     }
     `);
-  });
-
-
+});
 
 document.getElementById("back_button").addEventListener("click", goBack);
 document.getElementById("forward_button").addEventListener("click", goForward);
 document.getElementById("home_button").addEventListener("click", goHome);
 document.getElementById("go_button").addEventListener("click", go);
 document.getElementById("url_bar").addEventListener("keypress", function (e) {
-  if (e.key === "Enter"){
+  if (e.key === "Enter") {
     go();
   }
-})
+});
 
-
-function goHome()
-{
-  document.getElementById("browser").loadURL("https://search.nnisarg.xyz");
+function goHome() {
+  document.getElementById("browser").loadURL("https://searx.work");
 }
 
-function goBack()
-{
+function goBack() {
   document.getElementById("browser").goBack();
 }
 
-function goForward()
-{
+function goForward() {
   document.getElementById("browser").goForward();
 }
 
-function go()
-{
+function go() {
   let string_searched = document.getElementById("url_bar").value;
-  
-  if (string_searched.startsWith("@y ")){
-    string_searched = "https://youtube.com/search?&q=" + string_searched.replace("@y ", "");
-  }
-  else if (string_searched.startsWith("@g ")){
-    string_searched = "https://google.com/search?&q=" + string_searched.replace("@g ", "");
-  }
-  else if (!string_searched.startsWith("http")){
+
+  if (string_searched.startsWith("@y ")) {
+    string_searched =
+      "https://youtube.com/search?&q=" + string_searched.replace("@y ", "");
+  } else if (string_searched.startsWith("@g ")) {
+    string_searched =
+      "https://google.com/search?&q=" + string_searched.replace("@g ", "");
+  } else if (!string_searched.startsWith("http")) {
     string_searched = "http://" + string_searched;
   }
 
